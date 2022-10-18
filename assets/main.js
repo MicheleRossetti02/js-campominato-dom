69c68540e6615c4d869edc9a9a0c0193140dd649
MicheleRossetti02 <michelerossetti07@gmail.com>
2022-10-18 16:02:12 +0200

    create bombe

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,13 @@
 // Consegna
-// L'utente clicca su un bottone che genererà una griglia di gioco quadrata. Ogni cella ha un numero progressivo, da 1 a 100.
+// Copiamo la griglia fatta ieri nella nuova repo e aggiungiamo la logica del gioco (attenzione: non bisogna copiare tutta la cartella dell'esercizio ma solo l'index.html, e le cartelle js/ css/ con i relativi script e fogli di stile, per evitare problemi con l'inizializzazione di git).
+
+
+
+// Attenzione:
+// **nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+// In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+// La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
 
 
 // selexione il bottone
@@ -7,23 +15,11 @@ let bottone = document.querySelector(".btn")
 
 let container = document.querySelector(".container")
 
+//seleziono il massimo numero di bombe
+const bombe = 16;
+
     // assegno un numero alle caselle
 
-
-    // funzione che genera le caselle
-    function casellegenerator(numero_caselle, containerEl,caselle_per_riga) {
-        
-        for (let i = 1; i <= numero_caselle; i++) {
-
-
-            let casella =`<div class="casella aqua"> ${i}  </div>`;
-            // casella.style.width =`calc(100% / ${caselle_per_riga})`
-            containerEl.innerHTML += casella;
-
-            // casellaMarkUp.innerText += numero_caselle;
-        }
-        
-    }
 
 // assegno un azione al bottone
 bottone.addEventListener("click", function(){
@@ -58,10 +54,17 @@ bottone.addEventListener("click", function(){
         
     }
 
+    const bombs = bombegenerator(1,numero_caselle_1)
+    console.log(bombs);
+
+    // Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
 
 
-    
+
 })
+
+
+
 
 //  const tabella1 = document.getElementById('griglia').value =1;
 // function setselctor(numero_caselle, containerEl) {
@@ -69,117 +72,36 @@ bottone.addEventListener("click", function(){
 // }
 
 
-
-
-
-// // selexione il bottone
-// let bottone2 = document.querySelector(".violet")
-
-// // assegno un azione al bottone
-// bottone2.addEventListener("click", function(){
-
-
-
-//    // struttura del container
-//     let container = document.querySelector(".container")
-
-//     // assegno un numero alle caselle
-//     let numero_caselle = 82;
-    
-//     // richiamo la function
-//     casellegenerator(numero_caselle, container);
-    
-
-//     // funzione che genera le caselle
-//     function casellegenerator(numero_caselle, containerEl) {
+    // funzione che genera le caselle
+    function casellegenerator(numero_caselle, containerEl,) {
         
-//         for (let i = 1; i < numero_caselle; i++) {
+        for (let i = 1; i <= numero_caselle; i++) {
 
 
-//             let casella =`<div class="casella_2 violet"> ${i}  </div>`;
-//             containerEl.innerHTML += casella;
+            let casella =`<div class="casella aqua"> ${i}  </div>`;
+            // casella.style.width =`calc(100% / ${caselle_per_riga})`
+            containerEl.innerHTML += casella;
 
-//             // casellaMarkUp.innerText += numero_caselle;
-//         }
+            // casellaMarkUp.innerText += numero_caselle;
+        }
         
-//     }
+    }
 
-//     let listaCelle = document.querySelectorAll(".casella_2");
+    function bombegenerator(min,max){
+        const bombs=[];
+        while(bombs.length !== 16){
+            
+            const bomba = generatorecasellabomba(min,max);
 
-//     for (let i = 0; i < listaCelle.length; i++) {
-//         let number = i+1;
-//         const casellaAttuale = listaCelle[i];
-//         casellaAttuale.addEventListener("click", function(){
-//             console.log('Ho cliccato sulla casella ' + number);
-//             casellaAttuale.classList.toggle("violet")
-//             casellaAttuale.classList.toggle("viola")
-//         })
-        
-//     }
+            if(!bombs.includes(bomba)){
+                bombs.push(bomba)
+            }
 
 
 
-
-
-    
-// })
-
-
-
-
-
-
-
-
-// // selexione il bottone
-// let bottone3 = document.querySelector(".green")
-
-// // assegno un azione al bottone
-// bottone3.addEventListener("click", function(){
-
-
-
-
-//    // struttura del container
-//     let container = document.querySelector(".container")
-
-//     // assegno un numero alle caselle
-//     let numero_caselle = 50;
-    
-//     // richiamo la function
-//     casellegenerator(numero_caselle, container);
-    
-
-//     // funzione che genera le caselle
-//     function casellegenerator(numero_caselle, containerEl) {
-        
-//         for (let i = 1; i < numero_caselle; i++) {
-
-
-//             let casella =`<div class="casella_3 green"> ${i}  </div>`;
-//             containerEl.innerHTML += casella;
-
-//             // casellaMarkUp.innerText += numero_caselle;
-//         }
-        
-//     }
-
-
-//     let listaCelle = document.querySelectorAll(".casella_3");
-
-//     for (let i = 0; i < listaCelle.length; i++) {
-//         let number = i+1;
-//         const casellaAttuale = listaCelle[i];
-//         casellaAttuale.addEventListener("click", function(){
-//             console.log('Ho cliccato sulla casella ' + number);
-//             casellaAttuale.classList.toggle("green")
-//             casellaAttuale.classList.toggle("verde")
-//         })
-        
-//     }
-
-
-
-
-    
-// })
+        }
+        return bombs
+    }
+    function generatorecasellabomba (min,max){
+        return Math.ceil(Math.random()*(max-min +1))
+    }
